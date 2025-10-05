b5f2a6b49c3b9ef0b8c757fd8683c5c63da82415
Maulik Sharma <maulik.sharma@gndsolutions.in>
2025-10-05 18:24:15 +0530

    splash screen working

--- app/splash.tsx
+++ app/splash.tsx
@@ -1,18 +1,24 @@
-// app/splash.tsx
 import { useEffect } from 'react';
 import { useRouter } from 'expo-router';
 import { LinearGradient } from 'expo-linear-gradient';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+import { Image } from 'expo-image';
 
 export default function Splash() {
   const router = useRouter();
+
   useEffect(() => {
     const t = setTimeout(() => router.replace('/login'), 1000);
     return () => clearTimeout(t);
   }, []);
+
   return (
     <LinearGradient colors={['#1e3a8a', '#2563eb']} className="flex-1 items-center justify-center">
-      <Text className="text-3xl font-bold text-white">Thinxlog</Text>
+      <Image
+        source={require('assets/images/logo_splash.png')} // 🖼️ replace with your image path
+        style={{ width: 250, height: 220 }} // adjust size as needed
+        contentFit="contain"
+      />
     </LinearGradient>
   );
 }
