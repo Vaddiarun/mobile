d58d85c115857a118183b7963ce60ffde7c71ad7
Maulik Sharma <maulik.sharma@gndsolutions.in>
2025-10-09 12:04:16 +0530

    history_delete_function

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,7 @@
 // app/index.tsx
 import { Redirect } from 'expo-router';
+import 'react-native-gesture-handler'; // if used
+global.Buffer = global.Buffer || require('buffer').Buffer;
 export default function Entry() {
   return <Redirect href="/splash" />;
 }
--- mmkv-storage/storage.ts
+++ mmkv-storage/storage.ts
@@ -1,43 +1,47 @@
-// storage.ts
 import { MMKV } from "react-native-mmkv";
-
+ 
 export const storage = new MMKV({
   id: "user-storage",
   encryptionKey: "gndthinxloguser-341", // 🔑 keep this secure
 });
-
+ 
 export const saveUser = (user: any) => {
   storage.set("user", JSON.stringify(user));
 };
-
+ 
 export const getUser = (): any | null => {
   const userData = storage.getString("user");
   return userData ? JSON.parse(userData) : null;
 };
-
+ 
 export const clearUser = () => {
   storage.delete("user");
 };
-
+ 
 export const saveTrip = (tripData: any) => {
   const existingTrips = getTrips() || [];
   const trips = [...existingTrips, tripData];
   storage.set("trips", JSON.stringify(trips));
 };
-
+ 
 export const updateTrip = (deviceID: string, updates: any) => {
   const existingTrips = getTrips() || [];
-  const updatedTrips = existingTrips.map((trip: any) => 
+  const updatedTrips = existingTrips.map((trip: any) =>
     trip.deviceID === deviceID ? { ...trip, ...updates } : trip
   );
   storage.set("trips", JSON.stringify(updatedTrips));
 };
-
+ 
 export const getTrips = (): any | null => {
   const userData = storage.getString("trips");
   return userData ? JSON.parse(userData) : null;
 };
-
+ 
 export const clearTrip = () => {
   storage.delete("trips");
+};
+ 
+export const clearAllTrips = () => {
+  storage.delete("trips");
+  console.log('✅ All trips cleared from storage');
 };
